ef59881dd5cf4014d98f6782501df688f6d32b62
demianrenzulli <demian77@gmail.com>
2021-07-25 20:06:46 -0400

    SW events queries

---
+++ sw-events-by-url-quantity.sql
@@ -0,0 +1,38 @@
+#standardSQL
+# SW events by url
+CREATE TEMPORARY FUNCTION getSWEvents(swEventListenersInfo STRING)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+  var swEvents = Object.values(JSON.parse(swEventListenersInfo));
+  if (typeof swEvents == 'string') {
+    return [swEvents];
+  }
+  return swEvents;
+} catch (e) {
+  return [e];
+}
+''';
+SELECT
+  url,
+  COUNTIF(LOWER(sw_events) LIKE '%install%') AS install,
+  COUNTIF(LOWER(sw_events) LIKE '%activate%') AS activate,
+  COUNTIF(LOWER(sw_events) LIKE '%push%') AS push,
+  COUNTIF(LOWER(sw_events) LIKE '%notificationclick%') AS notificationclick,
+  COUNTIF(LOWER(sw_events) LIKE '%notificationclose%') AS notificationclose,
+  COUNTIF(LOWER(sw_events) LIKE '%sync%') AS sync,
+  COUNTIF(LOWER(sw_events) LIKE '%canmakepayment%') AS canmakepayment,
+  COUNTIF(LOWER(sw_events) LIKE '%paymentrequest%') AS paymentrequest,
+  COUNTIF(LOWER(sw_events) LIKE '%periodicsync%') AS periodicsync,
+  COUNTIF(LOWER(sw_events) LIKE '%backgroundfetchsuccess%') AS backgroundfetchsuccess,
+  COUNTIF(LOWER(sw_events) LIKE '%backgroundfetchfailure%') AS backgroundfetchfailure,
+  COUNTIF(LOWER(sw_events) LIKE '%backgroundfetchabort%') AS backgroundfetchabort,
+  COUNTIF(LOWER(sw_events) LIKE '%backgroundfetchclick%') AS backgroundfetchclick
+FROM
+  `httparchive.sample_data.pages_*`,
+  --`httparchive.pages.2021_07_01_*`,
+  UNNEST(getSWEvents(JSON_EXTRACT(payload, '$._pwa.swEventListenersInfo'))) AS sw_events
+WHERE
+  JSON_EXTRACT(payload, '$._pwa') != "[]" AND
+  JSON_EXTRACT(payload, '$._pwa.swEventListenersInfo') != "[]"
+GROUP BY url
+ORDER BY url ASC
