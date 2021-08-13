ab6b6f3daa500f74fc04da38d0d590233a3d95a0
demianrenzulli <demian77@gmail.com>
2021-08-13 15:34:31 -0400

    Install events

---
+++ sw-install-events-by-url-quantity.sql
@@ -0,0 +1,44 @@
+#standardSQL
+# SW install events by url
+CREATE TEMPORARY FUNCTION getInstallEvents(windowEventListeners ARRAY<STRING>, windowPropertiesInfo ARRAY<STRING>)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+  var installEvents = windowEventListeners.concat(windowPropertiesInfo);
+  return Array.from(new Set(installEvents));
+} catch (e) {
+  return [e];
+}
+''';
+CREATE TEMPORARY FUNCTION parseField(field STRING)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+    if(field == '[]' || field == '') {
+        return [];
+    }
+
+    var parsedField = Object.values(JSON.parse(field));
+
+    if (typeof parsedField != 'string') {
+        parsedField = parsedField.toString();
+    }
+
+    parsedField = parsedField.trim().split(',');
+    return parsedField;
+} catch (e) {
+  return [e];
+}
+''';
+SELECT
+  _TABLE_SUFFIX AS client,
+  url,
+  COUNTIF(LOWER(install_events) LIKE '%appinstalled%') AS appinstalled,
+  COUNTIF(LOWER(install_events) LIKE '%beforeinstallprompt%') AS beforeinstallprompt
+FROM
+  `httparchive.sample_data.pages_*`,
+  --`httparchive.pages.2021_07_01_*`,
+  UNNEST(getInstallEvents(parseField(JSON_EXTRACT(payload, '$._pwa.windowEventListenersInfo')), parseField(JSON_EXTRACT(payload, '$._pwa.windowPropertiesInfo')))) AS install_events
+WHERE
+  JSON_EXTRACT(payload, '$._pwa') != "[]" AND
+  (JSON_EXTRACT(payload, '$._pwa.windowEventListenersInfo') != "[]" OR JSON_EXTRACT(payload, '$._pwa.windowPropertiesInfo') != "[]")
+GROUP BY url, client, install_events
+ORDER BY url ASC
---
+++ sw-install-events-frequency.sql
@@ -0,0 +1,52 @@
+#standardSQL
+# SW install events frequency
+CREATE TEMPORARY FUNCTION getInstallEvents(windowEventListeners ARRAY<STRING>, windowPropertiesInfo ARRAY<STRING>)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+  var installEvents = windowEventListeners.concat(windowPropertiesInfo);
+  return Array.from(new Set(installEvents));
+} catch (e) {
+  return [e];
+}
+''';
+CREATE TEMPORARY FUNCTION parseField(field STRING)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+    if(field == '[]' || field == '') {
+        return [];
+    }
+
+    var parsedField = Object.values(JSON.parse(field));
+
+    if (typeof parsedField != 'string') {
+        parsedField = parsedField.toString();
+    }
+
+    parsedField = parsedField.trim().split(',');
+    return parsedField;
+} catch (e) {
+  return [e];
+}
+''';
+SELECT
+  client,
+  COUNT(0) AS freq,
+  COUNTIF(LOWER(install_events) LIKE '%appinstalled%') AS appinstalled,
+  COUNTIF(LOWER(install_events) LIKE '%beforeinstallprompt%') AS beforeinstallprompt
+FROM
+(
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url,
+    install_events
+  FROM
+    `httparchive.sample_data.pages_*`,
+    --`httparchive.pages.2021_07_01_*`,
+    UNNEST(getInstallEvents(parseField(JSON_EXTRACT(payload, '$._pwa.windowEventListenersInfo')), parseField(JSON_EXTRACT(payload, '$._pwa.windowPropertiesInfo')))) AS install_events
+  WHERE
+    JSON_EXTRACT(payload, '$._pwa') != "[]" AND
+    (JSON_EXTRACT(payload, '$._pwa.windowEventListenersInfo') != "[]" OR JSON_EXTRACT(payload, '$._pwa.windowPropertiesInfo') != "[]")
+  GROUP BY url, client, install_events
+  ORDER BY url ASC
+)
+GROUP BY client
